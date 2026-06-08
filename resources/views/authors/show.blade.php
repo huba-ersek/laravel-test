4a2b3760addf9d4f4999f38453fa6c0fc0f7e858
3116TAN-20 <3116TAN-20@tanuzem.boronkay.local>
2026-06-08 11:32:41 +0200

    forgot closing tag

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -5,6 +5,6 @@
 <h1>Író részletei</h1>
 
 <p><b>Név: </b>{{ $author->name }}</p>
-<p><b>Születési dátum: </b>{{ $author->birth_time }}<p>
+<p><b>Születési dátum: </b>{{ $author->birth_time }}</p>
 
 @endsection
