fe9dd48f4b98b91eb973cbc2adad24a3361523f5
3116TAN-20 <3116TAN-20@tanuzem.boronkay.local>
2026-06-08 10:27:02 +0200

    defined relationships between models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
