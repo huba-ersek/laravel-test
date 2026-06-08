717ed01343c3784e50b6e25561d48e4d560c257f
3041TAN-11 <3041TAN-11@tanuzem.boronkay.local>
2026-06-08 14:18:08 +0200

    better book inspection

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,9 +4,9 @@
 
 <h1>Könyv részletei</h1>
 
-<p><b>Író: </b>{{ $author->name }}</p>
+<p><b>Író: </b>{{ $book->author->name }}</p>
 <p><b>Cím: </b>{{ $book->title }}</p>
-<p><b>Műfaj: </b>{{ $genre->name }}</p>
+<p><b>Műfaj: </b>{{ $book->genre->name }}</p>
 <p><b>Kiadás éve: </b>{{ $book->year }}</p>
 <p><b>Leírás: </b>{{ $book->description }}</p>
 
